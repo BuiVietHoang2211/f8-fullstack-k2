67045bcb3578c093b9b1ea7c5f75da0bccdb39e9
BuiVietHoang2211 <buiviethoanga1@gmail.com>
2023-08-22 15:11:56 +0700

    BTVN 21

---
+++ JS/Day_21/js/ex1.js
@@ -0,0 +1,29 @@
+// Bài 1
+var errors = {
+  name: {
+    required: "Vui lòng nhập họ tên",
+    min: "Họ tên phải từ 5 ký tự",
+  },
+  email: {
+    email: "Định dạng email không hợp lệ",
+    unique: "Email đã có người sử dụng",
+    required: "Vui lòng nhập địa chỉ email",
+  },
+  password: {
+    required: "Vui lòng nhập mật khẩu",
+    same: "Mật khẩu phải khớp với mật khẩu nhập lại",
+  },
+};
+
+function getError(field) {
+  var error = errors[field];
+  for (var key in error) {
+    if (error.hasOwnProperty(key)) {
+      return error[key];
+    }
+  }
+}
+
+console.log(getError("name"));
+console.log(getError("email"));
+console.log(getError("password"));
